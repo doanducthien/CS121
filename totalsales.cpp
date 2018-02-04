043de4ceaf434e86c4e3e67516d67115796de50f
doanducthien <35688231+doanducthien@users.noreply.github.com>
2018-02-04 09:52:52 +0700

    Add files via upload

---
+++ totalsales.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string A[12] = {"Jan","Feb","Mar","Apr","May","Jun"."July","Aug","Sep","Oct","Nov","Dev"}
+	int i;
+	int tong = 0,sum[12];
+
+	for (i = 0;i < 12;i++)
+	{
+		cout<<"doanh thu thang"<<A[i+1]<<": \n";
+		cin>>sum[i]
+		tong += sum[i]
+
+	}
+	cout<<"tong sach ban dc trong 1 nam la: \n";
+	cout<<tong<<endl;
+	return 0;
+}
